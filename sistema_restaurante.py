f85196cb1eec93685cf60f4505a43255dcf0a815
calvo01 <calvo.felipe61@gmail.com>
2026-07-14 13:14:42 -0300

    restaurante com list

--- sistema_restaurante.py
+++ sistema_restaurante.py
@@ -3,17 +3,22 @@ opcao = 0
 realizados = 0
 maior_pedido = 0
 menor_pedido = 0
+maior_nome = 0
 
+nomes = []
+valores = []
 while opcao != 7:
     opcao = int(input("Selecione uma opção: 1- novo pedido, 2- faturamento, 3- quantidade de pedidos, 4- ticket medio,5- maior compra, 6- resumo do dia 7- encerrar: "))
     if opcao == 1:
         nome = input("nome cliente?")
+        nomes.append(nome)
         valor = int(input("qual valor do pedido?"))
+        valores.append(valor)
         faturamento = faturamento + valor
         realizados = realizados + 1
         if valor > maior_pedido:
             maior_pedido = valor
-            nome = maior_nome
+            maior_nome = nome
         if valor < menor_pedido or menor_pedido == 0:
             menor_pedido = valor
     elif opcao == 2:
@@ -31,7 +36,7 @@ while opcao != 7:
     elif opcao == 6:
         print(f"Faturamento do dia: R${faturamento}, quantidade de pedidos: {realizados}, maior pedido: R${maior_pedido})")
     elif opcao == 7:
-        print("encerrar")
+        print(nomes, valores)
         
     else:
         print ("opcao invalida")
