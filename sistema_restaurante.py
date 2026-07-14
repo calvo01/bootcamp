8e370bae5dcdc156ddb4aa5b90a7cfdac5fdb253
calvo01 <calvo.felipe61@gmail.com>
2026-07-14 13:23:29 -0300

    refatoração restaurante

--- sistema_restaurante.py
+++ sistema_restaurante.py
@@ -7,12 +7,14 @@ maior_nome = 0
 
 nomes = []
 valores = []
-while opcao != 7:
-    opcao = int(input("Selecione uma opção: 1- novo pedido, 2- faturamento, 3- quantidade de pedidos, 4- ticket medio,5- maior compra, 6- resumo do dia 7- encerrar: "))
+
+
+while opcao != 8:
+    opcao = int(input("Selecione uma opção: 1- Novo Pedido, 2- Faturamento, 3- Quantidade de Pedidos, 4- Ticket Médio, 5- Maior Compra, 6- Resumo do Dia, 7-Lista de Compras 8- Encerrar: "))
     if opcao == 1:
-        nome = input("nome cliente?")
+        nome = input("Qual o nome do cliente?")
         nomes.append(nome)
-        valor = int(input("qual valor do pedido?"))
+        valor = int(input("Qual o valor do pedido?"))
         valores.append(valor)
         faturamento = faturamento + valor
         realizados = realizados + 1
@@ -22,21 +24,23 @@ while opcao != 7:
         if valor < menor_pedido or menor_pedido == 0:
             menor_pedido = valor
     elif opcao == 2:
-        print(f"{faturamento}")
+        print(f"O faturamento do dia foi de R${faturamento}")
     elif opcao == 3:
-        print(f"{realizados}")
+        print(f" A quantidade de pedidos realizados foi de {realizados}")
     elif opcao == 4:
         if realizados < 1:
-            print("nenhum pedido ainda")
+            print("Nenhum pedido realizado, não é possível calcular o ticket médio")
         else:
             medio = faturamento / realizados   
-            print(f"{medio}")
+            print(f"O ticket médio do dia foi de R${medio}")
     elif opcao == 5:
         print(f"O maior pedido foi de R${maior_pedido}, do cliente {maior_nome}")
     elif opcao == 6:
         print(f"Faturamento do dia: R${faturamento}, quantidade de pedidos: {realizados}, maior pedido: R${maior_pedido})")
     elif opcao == 7:
-        print(nomes, valores)
+        print(f"Clientes: {nomes}, Valores: {valores}")
+    elif opcao == 8:
+        print("Programa Encerrado")
         
     else:
         print ("opcao invalida")
